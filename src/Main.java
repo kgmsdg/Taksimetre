b881102b8a3f3ecfcc7314243389d7d83ca54132
Kaan Gumusdag <uveyskaangumusdag@hotmail.com>
2021-10-12 15:21:07 +0300

    taksimetree

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+      int km;
+      double perKm = 2.20, total, stPrc = 10;
+
+      Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi KM cinsinden giriniz : ");
+        km = input.nextInt();
+
+        total = (km * perKm);
+        total += stPrc;
+
+        total = (total < 20) ? 20 : total;
+        System.out.println("Toplam tutar : " + total);
+
+    }
+    }
